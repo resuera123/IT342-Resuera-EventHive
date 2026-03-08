23978a636517c728c52c69b0f47c4e8beaee4795
LAB24_USER ON WS37 <L24X07W37@LabHCI.net>
2026-03-09 07:49:08 +0800

    Backend Initialization

---
+++ backend/eventhive/src/test/java/edu/cit/resuera/eventhive/EventhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.resuera.eventhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
